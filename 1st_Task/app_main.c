285cc457cc621329b558ed5f9a3d9ca61a8a9c7b
Muhamedhagag <47712323+Muhamedhagag@users.noreply.github.com>
2021-02-28 02:39:44 +0200

    1st Task

---
+++ 1st_Task/app_main.c
@@ -0,0 +1,26 @@
+#include "app_main.h"
+
+char u8InitGLobalVar = 2;
+static long int u32NonInitStaticGlobalVar; 
+const short int u16ConstGlobalVar = 3;
+char arr[3]  __attribute__((__section__(".sprints"))) = {0};
+char arr2[2]  __attribute__((__section__(".sprints"))) = {0};
+int x __attribute__((__section__(".sprints"))) = 0;
+void main(void)
+{
+	short int u16LocVar = 4;
+	static char u8InitStaticLocVar = 5;
+	if(u16ConstGlobalVar == 3)
+	{
+		u32NonInitStaticGlobalVar = 6;
+		vidFunc(&u16LocVar);
+	}
+	else
+	{
+		u32NonInitStaticGlobalVar = 7;
+	}
+	while(1)
+	{
+	
+	}
+}
